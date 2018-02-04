801a5a6ce4bc4e9db28912dc921e1fa803f72416
Stefan Lieberth <stefan@lieberth.net>
2018-02-04 13:22:02 +0100

    .

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-#sys.path.insert(0, os.path.abspath('../aioRunbook/'))
-sys.path.insert(0, os.path.abspath('../aioRunbook/aioRunbook'))
+sys.path.insert(0, os.path.abspath('../aioRunbook/'))
+sys.path.insert(0, os.path.abspath('../aioRunbook/aioRunbook/'))
 
 
 
